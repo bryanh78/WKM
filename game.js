2ff1bf5c03d65e9f91c2d8c86f9d2f6e96f00e58
Student <student@RU-008.local>
2016-02-09 17:16:34 -0700

    working on filling modal

--- game.js
+++ game.js
@@ -7,12 +7,56 @@ angular.module("game")
 			var s = $scope
 			s.activeRooms = "mainScreen"
 			s.person = []
+			s.selectedThing = []
 			// s.enterGame = function() {
 			// 	s.mainScreen = !s.mainScreen;
 			// 	s.mainHall = !s.mainHall;
 
 			// }
-			
+			// s.inverntory = function(event, items) {
+			// 	console.log(items)
+			// 	s.bag = 
+			// }
+			s.interact=function(event){
+				console.log(event)
+				s.selectedThing.push(event)
+				console.log(s.selectedThing)
+			}
+
+			s.collect = function (item) {
+				s.person.push(item)
+			}
+
+			s.items = {
+				key : {
+					name : "Small Key",
+					holding : true,
+					description : "A small, simply designed key. It wouldn't be used for a safe. I wonder what it's for..."
+				},
+				flashlight : {
+					name : "Flashlight",
+					holding : true,
+					description : "I wonder how long this has been here. Who knows how much battery it has left."
+				},
+				firstnote : {
+					name : "Note",
+					holding : true,
+					description : "438... of course its torn.",
+					back : "U... hmmmm"
+				},
+				secondnote : {
+					name : "Note",
+					holding : true,
+					description : "266... looks like im only missing one more piece.",
+					back : "N..."
+				},
+				thirdnote : {
+					name : "Note",
+					holding : true,
+					description : "175... I wonder what order these go in?",
+					back : "R... ooookkkk? U.N.R? or maybe it's..."
+				}
+			}
 
 			s.changeRoom = function(event) {
 				s.activeRooms = event
@@ -24,7 +68,7 @@ angular.module("game")
 					img : "http://vignette2.wikia.nocookie.net/creepypasta/images/0/01/Creepy_house-wallpaper-1600x900.jpg/revision/latest?cb=20121007054213"
 				},
 				door : {
-					img : "https://s-media-cache-ak0.pinimg.com/236x/0a/31/10/0a311082738a18787f129b1dcaad041e.jpg"
+					img : "https://i.ytimg.com/vi/gYEGP6IcfPw/maxresdefault.jpg"
 				},
 				mainHall : {
 					img : "http://farm8.staticflickr.com/7236/7218668744_65e2ee513a_b.jpg"
